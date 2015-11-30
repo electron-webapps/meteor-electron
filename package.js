7db4bb8fb069eaf18fa597a820d039a480fa2a14
Jeff Wear <jeff@mixmax.com>
2015-11-30 11:57:16 -0800

    Remove unused files

--- package.js
+++ package.js
@@ -16,18 +16,10 @@ Npm.depends({
 });
 
 Package.on_use(function (api, where) {
-  // api.use("sanjo:meteor-files-helpers@1.1.0_7", "server");
   api.use(["mongo-livedata", "webapp", "ejson", "underscore"], "server");
   api.use(["iron:router"], {weak: true});
   api.add_files(['server.js'], 'server')
-  api.add_files(['index.js'], 'server', {isAsset: true});
   api.add_files([
-    // "electron-v0.31.0-darwin-x64.zip",
-    // "electron-v0.31.0-linux-arm.zip",
-    // "electron-v0.31.0-linux-ia32.zip",
-    // "electron-v0.31.0-linux-x64.zip",
-    // "electron-v0.31.0-win32-ia32.zip",
-    // "electron-v0.31.0-win32-x64.zip",
     "app/package.json",
     "app/main.js"
   ], "server", {isAsset: true});
