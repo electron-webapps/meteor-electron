6c798ba506c36389445710076eb1767f6674966f
Mike Risse <rissem@gmail.com>
2015-12-09 11:48:52 -0800

    allow multiple platform builds to be triggered from Meteor settings

--- server/createBinaries.js
+++ server/createBinaries.js
@@ -12,18 +12,18 @@ var exec = Meteor.wrapAsync(function(command, options, callback){
   });
 });
 
-createBinaries = function() {
+var electronSettings = Meteor.settings.electron || {};
+
+var createBuildDirectories = function(build){
   // Use a predictable directory so that other scripts can locate the builds, also so that the builds
   // may be cached:
   // TODO(jeff): Use existing binaries if the app hasn't changed.
-  var workingDir = path.join(process.env.PWD, '.meteor-electron');
+
+  var workingDir = path.join(process.env.PWD, '.meteor-electron', build.platform + "-" + build.arch);
   mkdirp(workingDir);
 
-  //TODO probably want to allow users to add other more unusual
-  //architectures if they want (ARM, 32 bit, etc.)
-  var platform = "darwin";
-
-  //TODO seed the binaryDir from the package assets
+  //TODO consider seeding the binaryDir from package assets so package
+  //could work without an internet connection
 
   // *binaryDir* holds the vanilla electron apps
   var binaryDir = path.join(workingDir, "releases");
@@ -41,97 +41,129 @@ createBinaries = function() {
   var finalDir = path.join(workingDir, "final");
   mkdirp(finalDir);
 
-
-  var electronSettings = Meteor.settings.electron || {};
-  var appVersion = electronSettings.version;
-  var appName = electronSettings.name;
-
-  [
-    "autoUpdater.js",
-    "main.js",
-    "menu.js",
-    "package.json",
-    "preload.js",
-    "proxyWindowEvents.js"
-  ].forEach(function(filename) {
-    var fileContents = Assets.getText(path.join("app", filename));
-
-    // Replace parameters in `package.json`.
-    if (filename === "package.json") {
-      var packageJSON = JSON.parse(fileContents);
-      if (appVersion) packageJSON.version = appVersion;
-      if (appName) {
-        packageJSON.name = appName.toLowerCase().replace(/\s/g, '-');
-        packageJSON.productName = appName;
-      }
-      fileContents = JSON.stringify(packageJSON);
-    }
-
-    writeFile(path.join(appDir, filename), fileContents);
-  });
-
-  //TODO be smarter about caching this..
-  exec("npm install", {cwd: appDir});
-
-  var settings = _.defaults({}, electronSettings, {
-    rootUrl: process.env.ROOT_URL
-  });
-
-  var signingIdentity = electronSettings.sign;
-  if (canServeUpdates()) {
-    // Enable the auto-updater if possible.
-    if ((platform === 'darwin') && !signingIdentity) {
-      // If the app isn't signed and we try to use the auto-updater, it will
-      // throw an exception.
-      console.error('Developer ID signing identity is missing: remote updates will not work.');
-    } else {
-      settings.updateFeedUrl = settings.rootUrl + UPDATE_FEED_PATH;
-    }
+  return {
+    working: workingDir,
+    binary: binaryDir,
+    app: appDir,
+    build: buildDir,
+    final: finalDir
   }
+}
 
-  writeFile(path.join(appDir, "electronSettings.json"), JSON.stringify(settings));
-
+var packagerSettings = function(buildInfo, dirs){
   var packagerSettings = {
-    dir: appDir,
-    name: appName || "Electron",
-    platform: platform,
-    arch: "x64",
+    dir: dirs.app,
+    name: electronSettings.name || "Electron",
+    platform: build.platform,
+    arch: build.arch,
     version: "0.35.0",
-    out: buildDir,
-    cache: binaryDir,
+    out: dirs.build,
+    cache: dirs.binary,
     overwrite: true
   };
-  if (appVersion) {
-    packagerSettings['app-version'] = appVersion;
+
+  if (electronSettings.version) {
+    packagerSettings['app-version'] = electronSettings.version;
   }
   if (electronSettings.icon) {
-    var icon = platformSpecificSetting(electronSettings.icon);
+    var icon = platformSpecificSetting(electronSettings.icon, buildInfo.platform);
     if (icon) {
       var iconPath = path.join(process.cwd(), 'assets', 'app', icon);
       packagerSettings.icon = iconPath;
     }
   }
-  if (signingIdentity) {
-    packagerSettings.sign = signingIdentity;
+  if (electronSettings.sign) {
+    packagerSettings.sign = electronSettings.sign;
+  }
+  return packagerSettings;
+}
+
+/* Entry Point */
+createBinaries = function() {
+  var result = {};
+
+  if (electronSettings.builds){
+    var builds = electronSettings.builds;
+  } else {
+    //just build for the current platform/architecture
+    var builds = [{platform: process.platform, arch: process.arch}];
   }
 
-  var build = electronPackager(packagerSettings)[0];
-  console.log("Build created at", build);
+  builds.forEach(function(buildInfo){
+    var buildDirs = createBuildDirectories(buildInfo);
 
+    /* Write out Electron Settings */
+    var appVersion = electronSettings.version;
+    var appName = electronSettings.name;
+    var signingIdentity = electronSettings.sign;
 
-  // Package the build for download.
+    var settings = _.defaults({}, electronSettings, {
+      rootUrl: process.env.ROOT_URL
+    });
 
-  // The auto-updater framework only supports installing ZIP releases:
-  // https://github.com/Squirrel/Squirrel.Mac#update-json-format
-  var downloadName = "app-darwin.zip";
-  var compressedDownload = path.join(finalDir, downloadName);
+    if (canServeUpdates()) {
+      // Enable the auto-updater if possible.
+      if ((buildInfo.platform === 'darwin') && !signingIdentity) {
+        // If the app isn't signed and we try to use the auto-updater, it will
+        // throw an exception.
+        console.error('Developer ID signing identity is missing: remote updates will not work.');
+      } else {
+        settings.updateFeedUrl = settings.rootUrl + UPDATE_FEED_PATH;
+      }
+    }
+    writeFile(path.join(buildDirs.app, "electronSettings.json"), JSON.stringify(settings));
 
-  // Use `ditto` to ZIP the app because I couldn't find a good npm module to do it and also that's
-  // what a couple of other related projects do:
-  // - https://github.com/Squirrel/Squirrel.Mac/blob/8caa2fa2007b29a253f7f5be8fc9f36ace6aa30e/Squirrel/SQRLZipArchiver.h#L24
-  // - https://github.com/jenslind/electron-release/blob/4a2a701c18664ec668c3570c3907c0fee72f5e2a/index.js#L109
-  exec('ditto -ck --sequesterRsrc --keepParent "' + build + '" "' + compressedDownload + '"');
-  console.log("Downloadable created at", compressedDownload);
+    /* Write out Electron application files */
+    [
+      "autoUpdater.js",
+      "main.js",
+      "menu.js",
+      "package.json",
+      "preload.js",
+      "proxyWindowEvents.js"
+    ].forEach(function(filename) {
+      var fileContents = Assets.getText(path.join("app", filename));
 
-  return build;
+      // Replace parameters in `package.json`.
+      if (filename === "package.json") {
+        var packageJSON = JSON.parse(fileContents);
+        if (appVersion) packageJSON.version = appVersion;
+        if (appName) {
+          packageJSON.name = appName.toLowerCase().replace(/\s/g, '-');
+          packageJSON.productName = appName;
+        }
+        fileContents = JSON.stringify(packageJSON);
+      }
+
+      writeFile(path.join(buildDirs.app, filename), fileContents);
+    });
+
+    //TODO be smarter about caching this..
+    exec("npm install", {cwd: buildDirs.app});
+
+    /* Create Build */
+    var build = electronPackager(packagerSettings(buildInfo, buildDirs))[0];
+    console.log("Build created for ", buildInfo.platform, buildInfo.arch, "at", build);
+
+    /* Package the build for download. */
+
+    // The auto-updater framework only supports installing ZIP releases:
+    // https://github.com/Squirrel/Squirrel.Mac#update-json-format
+    if (appName){
+      var downloadName = appName.toLowercase() + "-" + buildInfo.platform + "-" + buildInfo.arch + ".zip";
+    } else {
+      var downloadName = "Electron-" + buildInfo.platform + "-" + buildInfo.arch + ".zip";
+    }
+
+    var compressedDownload = path.join(buildDirs.final, downloadName);
+
+    // Use `ditto` to ZIP the app because I couldn't find a good npm module to do it and also that's
+    // what a couple of other related projects do:
+    // - https://github.com/Squirrel/Squirrel.Mac/blob/8caa2fa2007b29a253f7f5be8fc9f36ace6aa30e/Squirrel/SQRLZipArchiver.h#L24
+    // - https://github.com/jenslind/electron-release/blob/4a2a701c18664ec668c3570c3907c0fee72f5e2a/index.js#L109
+    exec('ditto -ck --sequesterRsrc --keepParent "' + build + '" "' + compressedDownload + '"');
+    console.log("Downloadable created at", compressedDownload);
+    result[buildInfo.platform + "-" + buildInfo.arch] = build
+  });
+  return result;
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 if ((process.env.NODE_ENV === 'development') && (process.env.ELECTRON_AUTO_BUILD !== 'false')) {
-  var build = createBinaries();
-  launchApp(build);
+  var builds = createBinaries();
+  var buildForThisPlatform = builds[process.platform + "-" + process.arch];
+  if (buildForThisPlatform){
+    launchApp(buildForThisPlatform);
+  }
 }
 
 serveUpdateFeed();
