7fe049f9cdc98a5e5f5bb5a90026555bd9a25164
Jeff Wear <jeff@mixmax.com>
2015-12-17 00:37:19 -0800

    Only create ZIP on Mac
    The Windows update framework will use a different package format.

--- server/createBinaries.js
+++ server/createBinaries.js
@@ -154,25 +154,22 @@ createBinaries = function() {
     }
 
     /* Package the build for download. */
+    // TODO(rissem): make this platform independent
 
-    // The auto-updater framework only supports installing ZIP releases:
-    // https://github.com/Squirrel/Squirrel.Mac#update-json-format
-    var downloadName;
-    if (appName){
-      downloadName = appName.toLowerCase() + "-" + buildInfo.platform + "-" + buildInfo.arch + ".zip";
-   } else {
-      downloadName = "Electron-" + buildInfo.platform + "-" + buildInfo.arch + ".zip";
-    }
+    if (IS_MAC) {
+      // The auto-updater framework only supports installing ZIP releases:
+      // https://github.com/Squirrel/Squirrel.Mac#update-json-format
+      var downloadName = (appName || "app") + ".zip";
+      var compressedDownload = path.join(buildDirs.final, downloadName);
 
-    var compressedDownload = path.join(buildDirs.final, downloadName);
-
-    if (buildRequired || !exists(compressedDownload)) {
-      // Use `ditto` to ZIP the app because I couldn't find a good npm module to do it and also that's
-      // what a couple of other related projects do:
-      // - https://github.com/Squirrel/Squirrel.Mac/blob/8caa2fa2007b29a253f7f5be8fc9f36ace6aa30e/Squirrel/SQRLZipArchiver.h#L24
-      // - https://github.com/jenslind/electron-release/blob/4a2a701c18664ec668c3570c3907c0fee72f5e2a/index.js#L109
-      exec('ditto -ck --sequesterRsrc --keepParent "' + app + '" "' + compressedDownload + '"');
-      console.log("Downloadable created at", compressedDownload);
+      if (buildRequired || !exists(compressedDownload)) {
+        // Use `ditto` to ZIP the app because I couldn't find a good npm module to do it and also that's
+        // what a couple of other related projects do:
+        // - https://github.com/Squirrel/Squirrel.Mac/blob/8caa2fa2007b29a253f7f5be8fc9f36ace6aa30e/Squirrel/SQRLZipArchiver.h#L24
+        // - https://github.com/jenslind/electron-release/blob/4a2a701c18664ec668c3570c3907c0fee72f5e2a/index.js#L109
+        exec('ditto -ck --sequesterRsrc --keepParent "' + app + '" "' + compressedDownload + '"');
+        console.log("Downloadable created at", compressedDownload);
+      }
     }
 
     results[buildInfo.platform + "-" + buildInfo.arch] = {
